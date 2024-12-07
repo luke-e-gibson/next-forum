cde3d12df146c7ea2e4442675e2fcf04c2339a11
luke-e-gibson <luke.e.gibson@outlook.com>
2024-12-08 10:49:35 +1100

    Add database schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,36 +1,24 @@
-// Example model schema from the Drizzle docs
-// https://orm.drizzle.team/docs/sql-schema-declaration
-
+import exp from "constants";
 import { sql } from "drizzle-orm";
-import {
-  index,
-  integer,
-  pgTableCreator,
-  timestamp,
-  varchar,
-} from "drizzle-orm/pg-core";
+import { index, integer, pgTable, pgTableCreator, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
-/**
- * This is an example of how to use the multi-project schema feature of Drizzle ORM. Use the same
- * database instance for multiple projects.
- *
- * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
- */
-export const createTable = pgTableCreator((name) => `forum_${name}`);
+export const subfourm = pgTable("subfourm", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+});
+export const subfourm_post = pgTable("subfourm_post", {
+  id: serial("id").primaryKey(),
+  subfourm_id: integer("subfourm_id").notNull().references(() => subfourm.id),
+  title: text("title").notNull(),
+  description: text("description").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+});
 
-export const posts = createTable(
-  "post",
-  {
-    id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at", { withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
-      () => new Date()
-    ),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
+export const post_comment = pgTable("post_comment", {
+  id: serial("id").primaryKey(),
+  post_id: integer("post_id").notNull().references(() => subfourm_post.id),
+  comment: text("comment").notNull(),
+  created_at: timestamp("created_at").notNull().defaultNow(),
+});
